73fa8235bfd483f2058bb5288068a3cb7f55e1f6
louise1991 <lelivrelouise@gmail.com>
2024-07-23 23:46:52 +0200

    solve reducer error

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './reducers/userReducer';
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
   },
 });
 
