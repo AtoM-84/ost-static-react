abaf3c173647ef69c9ece1041e5c46ade4b55d55
louise1991 <lelivrelouise@gmail.com>
2024-07-27 15:33:33 +0200

    action dispatch for burger menu button

--- src/components/elements/Footer/Footer.tsx
+++ src/components/elements/Footer/Footer.tsx
@@ -1,48 +1,37 @@
-import { Menu, MenuItem, Segment, Image } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
+import { Menu, MenuItem, Segment, Image } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
 
-import './Footer.scss';
-import logo from '../../../assets/OST-logo 1.svg'
+import "./Footer.scss";
+import logo from "../../../assets/OST-logo 1.svg";
 
 export default function Footer() {
-  return (
-    <Segment inverted id="footer">
-      <Menu stackable inverted className="footer__menu">
-        <MenuItem
-          className="footer__logo"
-          name='logo'
-          source={logo}
-          // as={NavLink}
-          // to="/"
-        >
-           <Image id="header__img" src={logo} size='tiny' />
-        </MenuItem>
+    return (
+        <Segment inverted id="footer">
+            <Menu stackable inverted className="footer__menu">
+                <MenuItem
+                    className="footer__logo"
+                    name="logo"
+                    source={logo}
+                    // as={NavLink}
+                    // to="/"
+                >
+                    <Image id="header__img" src={logo} size="tiny" />
+                </MenuItem>
 
-        <MenuItem
-          className="footer__item text-corpus"
-          name="legal notices"
-          as={NavLink}
-          to="/legal-notice"
-        >
-          Mentions Légales
-        </MenuItem>
+                <MenuItem className="footer__item text-corpus" name="legal notices" as={NavLink} to="/legal-notice">
+                    Mentions Légales
+                </MenuItem>
 
-        <MenuItem
-          className="footer__item text-corpus"
-          name="CGU"
-          as={NavLink}
-          to="/general-conditions-use"
-        >
-          Conditions Générales d&apos;Utilisation
-        </MenuItem>
+                <MenuItem className="footer__item text-corpus" name="CGU" as={NavLink} to="/general-conditions-use">
+                    Conditions Générales d&apos;Utilisation
+                </MenuItem>
 
-        <MenuItem className="footer__contact text-corpus" name="Contact">
-          <span>
-            Contactez-nous&nbsp;:{' '}
-            <a href="mailto:outerspacetech.io">outer-space-tech.io</a>
-          </span>
-        </MenuItem>
-      </Menu>
-    </Segment>
-  );
+                <MenuItem className="footer__contact text-corpus" name="Contact">
+                    <span>
+                        Contactez-nous&nbsp;: <a href="mailto:outerspacetech.io">outer-space-tech.io</a>
+                    </span>
+                </MenuItem>
+            </Menu>
+        </Segment>
+    );
 }
--- src/components/elements/Header/HeaderOST.tsx
+++ src/components/elements/Header/HeaderOST.tsx
@@ -1,45 +1,41 @@
-import {
-  Menu,
-  MenuItem, 
-  Segment,
-  Image,
-} from 'semantic-ui-react'
+import { Menu, MenuItem, Segment, Image, Button } from "semantic-ui-react";
 
-import { NavLink } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../../../features/uiSlice/uiSlice";
 
-import './HeaderOST.scss'
-import logo from '../../../assets/OST-logo 1.svg'
+import { NavLink } from "react-router-dom";
+
+import "./HeaderOST.scss";
+import logo from "../../../assets/OST-logo 1.svg";
 
 export default function HeaderOST() {
-  return (
-  <nav>
-    <Segment inverted className="header">
-      <Menu inverted secondary stackable size='massive'>
-        <MenuItem>
-        <Image id="header__img" src={logo} />
-        </MenuItem>
-      </Menu>
-    
-      <Menu inverted secondary stackable size='massive' id='nav__menu' >
-        <MenuItem
-          className="header__item text-chapo"
-          name="Accueil"
-          as={NavLink}
-          to="/"
-        >
-          Home
-        </MenuItem>
+    const dispatch = useDispatch();
+    function handleClick() {
+        dispatch(toggleMenu());
+        console.log("burger");
+    }
 
-        <MenuItem
-          className="header__item text-chapo"
-          name="Profil"
-          as={NavLink}
-          to="/Contact"
-        >
-          Contact
-        </MenuItem>
+    return (
+        <nav>
+            <Segment inverted className="header">
+                <Menu inverted secondary stackable size="massive">
+                    <MenuItem>
+                        <Image id="header__img" src={logo} />
+                    </MenuItem>
+                </Menu>
 
-        {/* <MenuItem
+                <Button onClick={handleClick}>Burger</Button>
+
+                <Menu inverted secondary stackable size="massive" id="nav__menu">
+                    <MenuItem className="header__item text-chapo" name="Accueil" as={NavLink} to="/">
+                        Home
+                    </MenuItem>
+
+                    <MenuItem className="header__item text-chapo" name="Profil" as={NavLink} to="/Contact">
+                        Contact
+                    </MenuItem>
+
+                    {/* <MenuItem
           className="header__item text-chapo"
           name="Profil"
           as={NavLink}
@@ -47,14 +43,12 @@ export default function HeaderOST() {
         >
           Blog
         </MenuItem> */}
-      </Menu>
-    </Segment>
-  
-       {/* <button className='burger__bar'>
+                </Menu>
+            </Segment>
+
+            {/* <button className='burger__bar'>
          <span></span>
        </button> */}
-   
-  </nav>
-   
-  );
+        </nav>
+    );
 }
