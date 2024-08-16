cd93bfd90b3da7e0978a35e3278dbef7e2ee2897
louise1991 <lelivrelouise@gmail.com>
2024-08-16 13:13:47 +0200

    postSlice praepared for fetch API with axios

---
+++ src/features/postSlice/postsSlice.ts
@@ -0,0 +1,42 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { Post } from '../../@types/post';
+import axios from 'axios';
+
+interface PostsState {
+    list: Post[];
+    isLoading: boolean,
+    error: string | undefined | null
+}
+
+export const initialState: PostsState = {
+    list: [],
+    isLoading: false, 
+    error: "",
+};
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/blog`);
+    return data; 
+}) 
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    
+  },
+  extraReducers(builder) {
+    builder
+    .addCase(fetchPosts.pending, (state) => {
+        state.isLoading = true;
+    }).addCase(fetchPosts.rejected, (state, action) => {
+        state.error = action.error.message;
+        state.isLoading = false;
+    }).addCase(fetchPosts.fulfilled, (state, action) => {
+        state.list = action.payload;
+        state.isLoading = false; 
+    })
+  }
+})
+
+export default postsSlice.reducer; 
