00039f97653eb4772ed515a7472324f072e95f14
louise1991 <lelivrelouise@gmail.com>
2024-09-21 13:44:18 +0200

    modify post type

--- src/@types/post.d.ts
+++ src/@types/post.d.ts
@@ -1,8 +1,11 @@
-export interface Post {
+export interface PostType {
     id: number
     title: string
     sub_title: string
     updated_at: number | string 
-    author_name: string
-    author_email: string 
+    author_name: [string]
+    // author_email: string 
+    content: string
+    status: "idle" | "pending" | "rejected" | "fulfilled"
+    error: string | undefined | null
 }
